46d0d0eaf647d1645319d27a769a18d96dd43d01
quangvupp321 <39510425+quangvupp321@users.noreply.github.com>
2019-11-10 19:28:05 +0700

    Fix interface MainActivity, add checkValid TourList

--- app/src/main/java/com/example/loginandregister/MainActivity.java
+++ app/src/main/java/com/example/loginandregister/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,8 +36,8 @@ import static com.example.loginandregister.LoginActivity.URL;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText row;
     private EditText page;
-    private EditText byname;
-    private EditText isDesc;
+    private Spinner byname;
+    private Spinner isDesc;
     private Button get;
     private Button logout;
     private ListView listView;
@@ -51,15 +54,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         get.setOnClickListener(this);
         logout.setOnClickListener(this);
 
-
-
     }
 
     private void mapping() {
         row=findViewById(R.id.rowPerPage);
         page=findViewById(R.id.pageNum);
+
         byname=findViewById(R.id.orderBy);
+        ArrayAdapter<CharSequence> adapter_byname = ArrayAdapter.createFromResource(this, R.array.order_by, android.R.layout.simple_spinner_item);
+        adapter_byname.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        byname.setAdapter(adapter_byname);
+
         isDesc=findViewById(R.id.isDesc);
+        ArrayAdapter<CharSequence> adapter_isdesc = ArrayAdapter.createFromResource(this, R.array.is_desc, android.R.layout.simple_spinner_item);
+        adapter_isdesc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        isDesc.setAdapter(adapter_isdesc);
+
         get=findViewById(R.id.getlist);
         logout=findViewById(R.id.logout);
         //listView=findViewById(R.id.listview);
@@ -77,12 +87,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             case R.id.getlist:
             {
+                if (!checkEmpty(row, page)) {
+                    Toast.makeText(this, "Row or Page is empty", Toast.LENGTH_SHORT).show();
+                    break;
+                }
                 int mRow=Integer.parseInt(row.getText().toString());
                 int mPage=Integer.parseInt(page.getText().toString());
-                String mByname=byname.getText().toString();
-                Boolean mIsDesc=Boolean.parseBoolean(isDesc.getText().toString());
+                String mByname = byname.getSelectedItem().toString();
+                boolean mIsDesc;
+                if (isDesc.getSelectedItem().toString() == "Descending")
+                    mIsDesc = true;
+                else
+                    mIsDesc = false;
+
                 Map<String,String> map=new HashMap<>();
                 map.put("Authorization",token);
+                final int m_Row = mRow, m_Page = mPage;
                 Call<ListTour> call=jsonPlaceHolderApi.getTour(mRow,mPage,mByname,mIsDesc,map);
                 call.enqueue(new Callback<ListTour>() {
                     @Override
@@ -92,26 +112,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Toast.makeText(MainActivity.this,"Get khong thanh cong",Toast.LENGTH_SHORT).show();
                             return;
                         }
-                        arrayList=new ArrayList<>(response.body().getTotal());
-                        arrayList=response.body().getTourList();
+                        int total = response.body().getTotal();
+                        if (checkValidRow(m_Row, m_Page, total)) {
+                            arrayList = new ArrayList<>(total);
+                            arrayList = response.body().getTourList();
 
-                        Toast.makeText(MainActivity.this,"Get thanh cong ",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Get thanh cong ", Toast.LENGTH_SHORT).show();
+
+                            arrayList = new ArrayList<>(total);
+                            arrayList = response.body().getTourList();
+
+                            ListView lv = findViewById(R.id.listTour);
+                            CustomAdapter arrayAdapternew = new CustomAdapter(MainActivity.this, R.layout.custom_layout_tour_listview, arrayList);
 
 
-                        arrayList=new ArrayList<>(response.body().getTotal());
-                        arrayList=response.body().getTourList();
+                            //Sua cai nay ne, chuyen no thanh List View
 
-                        ListView lv=findViewById(R.id.listTour);
-                        CustomAdapter arrayAdapternew= new CustomAdapter(MainActivity.this,R.layout.custom_layout_tour_listview,arrayList);
+                            arrayAdapternew.notifyDataSetChanged();
+                            lv.setAdapter(arrayAdapternew);
 
-
-                        //Sua cai nay ne, chuyen no thanh List View
-
-                        arrayAdapternew.notifyDataSetChanged();
-                        lv.setAdapter(arrayAdapternew);
-
-                        Toast.makeText(MainActivity.this,"Get thanh cong ",Toast.LENGTH_SHORT).show();
-                        ////////////////////////////
+                            Toast.makeText(MainActivity.this, "Get thanh cong ", Toast.LENGTH_SHORT).show();
+                            ////////////////////////////
+                        } else {
+                            Toast.makeText(MainActivity.this, "Nhap RowPerPage va Page Num khong dung", Toast.LENGTH_SHORT).show();
+                        }
                     }
 
                     @Override
@@ -129,5 +153,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             }
         }
+    }
+
+    private boolean checkEmpty(EditText row, EditText page) {
+        if (TextUtils.isEmpty(row.getText().toString()) || TextUtils.isEmpty(page.getText().toString()))
+            return false;
+        return true;
+    }
+
+    private boolean checkValidRow(int mRow, int mPage, int total) {
+        double temp = total/(mPage-1);
+        int max_row = (int) Math.round(temp);
+        if (mRow <= max_row) {
+            return true;
+        }
+        return false;
     }
 }
